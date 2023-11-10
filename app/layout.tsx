c5c538db95db3a10eda9ab83a722725a8d7687b2
Notlivy <117109649+Notlivy@users.noreply.github.com>
2023-11-10 00:26:10 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "GGBOND海归通",
+  description: " 专注于解决海外留学生作业问题",
   viewport: {
     width: "device-width",
     initialScale: 1,
